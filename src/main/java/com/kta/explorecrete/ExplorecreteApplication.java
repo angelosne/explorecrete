fdfed941427c513d3e0578ef5dc3920d0cc47816
angel <negkas.aggelos@gmail.com>
2020-04-13 20:41:36 +0300

    Initial commit

---
+++ src/main/java/com/kta/explorecrete/ExplorecreteApplication.java
@@ -0,0 +1,13 @@
+package com.kta.explorecrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecreteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplorecreteApplication.class, args);
+    }
+
+}
