2f1242eafb27587ee34d313ea70eedbc7a53ccd1
angel <negkas.aggelos@gmail.com>
2020-05-22 16:35:04 +0300

    Show tour packages at index.html, create margin-bottom for each one

--- src/main/java/com/kta/explorecrete/controller/IndexController.java
+++ src/main/java/com/kta/explorecrete/controller/IndexController.java
@@ -1,16 +1,28 @@
 package com.kta.explorecrete.controller;
 
+import com.kta.explorecrete.entity.TourPackage;
+import com.kta.explorecrete.service.TourPackageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
+
+    TourPackageService tourPackageService;
+
+    public IndexController(TourPackageService tourPackageService) {
+        this.tourPackageService = tourPackageService;
+    }
 
     @GetMapping("/")
     public String welcome(Model model) {
         model.addAttribute("welcomeMessage", "Καλώς ορίσατε στο KTA Tours™");
         model.addAttribute("chooseDestination", "Επιλέξτε κάποιον από τους προορισμούς μας:");
+        List<TourPackage> tourPackages = tourPackageService.findAll();
+        model.addAttribute("tourPackages", tourPackages);
         return "index";
     }
 }
