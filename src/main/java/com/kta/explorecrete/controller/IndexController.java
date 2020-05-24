778c651632dedcac51f9e5fa0ec59fb06b484eb1
angel <negkas.aggelos@gmail.com>
2020-05-24 17:18:35 +0300

    fix top banner behind title. add contact.html

--- src/main/java/com/kta/explorecrete/controller/IndexController.java
+++ src/main/java/com/kta/explorecrete/controller/IndexController.java
@@ -30,4 +30,9 @@ public class IndexController {
     public String about(){
         return "about";
     }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }
